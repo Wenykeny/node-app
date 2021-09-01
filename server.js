72b0f8ecbc081cf1756927599a8af91873191f80
Blackbeard <moisesnt2@gmail.com>
2021-09-01 23:35:47 +0100

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcome! This is BlackBeard\'s web server...')
 })
 
 app.listen(port, () => {
